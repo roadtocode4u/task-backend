48e45628611e4f3cb0a945e99700184d92ca8200
Suraj Shende <surajshende247@gmail.com>
2022-06-30 20:52:32 +0530

    Port from ENV

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
-
+require('dotenv').config()
 const app = express();
 
 app.use(express.json());
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // temp database
 let tasks = [];
