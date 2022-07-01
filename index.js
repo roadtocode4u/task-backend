cd5393b0a00a8e03caeb8df4beafcacba0868ad4
Suraj Shende <surajshende247@gmail.com>
2022-07-01 20:19:39 +0530

    Mongo URI from env file

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const PORT = process.env.PORT || 5000;
 // temp database
 let tasks = [];
 
-const DB = "mera database";
+const DB = process.env.MONGODB_URI;
+console.log(DB)
 
 // Create a Task
 app.post("/tasks", (req, res) => {
